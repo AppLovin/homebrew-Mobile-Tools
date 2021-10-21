4f1c72ef7c4337fca42247988bf31ed0fcfc61e9
maximilianbuck <66306147+maximilianbuck@users.noreply.github.com>
2021-10-21 19:56:48 +0200

    update aldroid version to 0.3.0

--- Formula/aldroid.rb
+++ Formula/aldroid.rb
@@ -1,5 +1,5 @@
 class Aldroid < Formula
-    version "0.2.2"
+    version "0.3.0"
 
     desc "This tool transforms Android APKs to make them debuggable and MITMable/Charlesable"
     homepage "https://github.com/AppLovin/homebrew-Mobile-Tools"
