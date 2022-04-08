98eb37106ed569ab19d1f300702ee80263ebf6eb
maximilianbuck <66306147+maximilianbuck@users.noreply.github.com>
2022-04-08 10:06:29 +0200

    feat/aldroid_install_no-incremental_flag

--- Formula/aldroid.rb
+++ Formula/aldroid.rb
@@ -1,5 +1,5 @@
 class Aldroid < Formula
-    version "0.3.3"
+    version "0.3.4"
 
     desc "This tool transforms Android APKs to make them debuggable and MITMable/Charlesable"
     homepage "https://github.com/AppLovin/homebrew-Mobile-Tools"
