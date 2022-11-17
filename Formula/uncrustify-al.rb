8c1828528db238f50975c90ef59d737a467161e4
Harry Arakkal <harry.arakkal@applovin.com>
2022-11-17 11:22:03 -0700

    config/add_uncrustify_al_formula

---
+++ Formula/uncrustify-al.rb
@@ -0,0 +1,11 @@
+class UncrustifyAL < Formula
+    version "0.1.0"
+
+    desc "This is a version of the uncrustify tool built off of that repo's latest master."
+    homepage "https://github.com/AppLovin/homebrew-Mobile-Tools"
+    url "https://github.com/AppLovin/homebrew-Mobile-Tools/releases/download/uncrustify-al-v0.1.0/uncrustify-al-v0.1.0.tar.gz"
+
+    def install
+        bin.install "uncrustify-al"
+    end
+end
