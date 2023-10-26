59d40f33bc0ca960c07f796358367e368f7cafb2
Alan Cao <alan.cao@applovin.com>
2023-10-26 15:45:24 -0700

    Initial commit

---
+++ Casks/swiftformat-al.rb
@@ -0,0 +1,12 @@
+cask "swiftformat-al" do
+
+  version "0.1.0"
+  sha256 "f6ca221ada6eae684d2ce397c72900bbe7eb9f3674d8d33eff2c56d4df4f6810"
+
+  url "https://github.com/AppLovin/homebrew-Mobile-Tools/releases/download/swiftformat-al-v0.1.0/SwiftFormat-for-Xcode.zip"
+  name "swiftformat-al"
+  desc "This is a version of SwiftFormat for Xcode, an editor extension forked from https://github.com/nicklockwood/SwiftFormat"
+  homepage "https://github.com/AppLovin/homebrew-Mobile-Tools"
+
+  app "SwiftFormat for Xcode.app"
+end
