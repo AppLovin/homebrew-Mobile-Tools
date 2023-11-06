2eab2a163423a48f9d33e68502897764865f1a6d
Alan Cao <alan.cao@applovin.com>
2023-11-06 11:08:11 -0800

    Reordered stanzas as suggested by homebrew docs

--- Casks/swiftformat-al.rb
+++ Casks/swiftformat-al.rb
@@ -1,10 +1,11 @@
 cask "swiftformat-al" do
   version "0.1.0"
+  sha256 "0b953f35b253f88b2934dff820f75a95450b90c394188b82fbf7245b51c7ae58"
+  
+  url "https://github.com/AppLovin/homebrew-Mobile-Tools/releases/download/swiftformat-al-v#{version}/SwiftFormat-for-Xcode.zip"
   name "swiftformat-al"
   desc "This is a version of SwiftFormat for Xcode, an editor extension forked from https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/AppLovin/homebrew-Mobile-Tools/releases/download/swiftformat-al-v0.1.0/SwiftFormat-for-Xcode.zip"
- 
-  sha256 "0b953f35b253f88b2934dff820f75a95450b90c394188b82fbf7245b51c7ae58"
-
+  homepage "https://github.com/AppLovin/homebrew-Mobile-Tools"
+  
   app "SwiftFormat for Xcode.app"
 end
